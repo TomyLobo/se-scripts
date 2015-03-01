449d65d885986aa70a189f81f01e51eb89d220c6
TomyLobo <tomylobo@nurfuerspam.de>
2015-03-01 16:58:45 +0100

    Variablen umbenannt

--- AssemblerManager.cs
+++ AssemblerManager.cs
@@ -21,15 +21,15 @@ void cleanAssemblerOutput(IMyAssembler assembler, List<IMyTerminalBlock> contain
     // Solange der Assember noch nicht leer ist...
     while (assemblerInv.CurrentVolume != 0) {
         // ...suche den ersten leeren Container...
-        IMyInventory containerDestination = getFirstEmpty(containers);
+        IMyInventory containerInv = getFirstEmpty(containers);
 
-        if (containerDestination == null)
+        if (containerInv == null)
             return;
 
         // ...und transferiere alle Items dahin.
         List<IMyInventoryItem> assemblerItems = assemblerInv.GetItems();
         for (int i = assemblerItems.Count - 1; i >= 0; i--) {
-            assemblerInv.TransferItemTo(containerDestination, i, null, true, null);
+            assemblerInv.TransferItemTo(containerInv, i, null, true, null);
         }
     }
 }
@@ -39,26 +39,26 @@ void cleanAssemblerInput(IMyAssembler assembler, List<IMyTerminalBlock> containe
     if (assembler.IsProducing)
         return;
 
-    IMyInventory containerDestination = getFirstEmpty(containers);
+    IMyInventory containerInv = getFirstEmpty(containers);
 
-    if (containerDestination == null)
+    if (containerInv == null)
         return;
 
     IMyInventory assemblerInv = assembler.GetInventory(0);
     List<IMyInventoryItem> assemblerItems = assemblerInv.GetItems();
 
     for (int i = assemblerItems.Count -1; i >= 0; i--) {
-        assemblerInv.TransferItemTo(containerDestination, i, null, true, null);
+        assemblerInv.TransferItemTo(containerInv, i, null, true, null);
     }
 }
 
-float getPercent(IMyInventory inv) {
-    return ((float)inv.CurrentVolume / (float)inv.MaxVolume) * 100f;
+float getPercent(IMyInventory inventory) {
+    return ((float)inventory.CurrentVolume / (float)inventory.MaxVolume) * 100f;
 }
 
 
-bool IsFull(IMyInventory inv) {
-    if (getPercent(inv) >= 95)
+bool IsFull(IMyInventory inventory) {
+    if (getPercent(inventory) >= 95)
         return true;
     else
         return false;
@@ -68,9 +68,9 @@ IMyInventory getFirstEmpty(List<IMyTerminalBlock> containers) {
     // search our containers until we find an empty one
     for (int n = 0; n < containers.Count; n++) {
         IMyInventoryOwner container = containers[n] as IMyInventoryOwner;
-        IMyInventory containerInv = container.GetInventory(0);
-        if (!IsFull(containerInv)) {
-            return containerInv;
+        IMyInventory containerInventory = container.GetInventory(0);
+        if (!IsFull(containerInventory)) {
+            return containerInventory;
         }
     }
 
