0dcd5c454a31bbf20366f8486201659da65a7d65
TomyLobo <tomylobo@nurfuerspam.de>
2015-03-14 22:38:45 +0100

    Byte-Order-Mark, using-Direktiven und namespace entfernt.

--- utils/FloatVector3.cs
+++ utils/FloatVector3.cs
@@ -1,171 +1,163 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+public class FloatVector3 {
+    private float[] data = new float[3];
 
-namespace opMatrix {
-    public class FloatVector3 {
-        private float[] data = new float[3];
+    public float x {
+        get { return data[0]; }
+        set { data[0] = value; }
+    }
 
-        public float x {
-            get { return data[0]; }
-            set { data[0] = value; }
+    public float y {
+        get { return data[1]; }
+        set { data[1] = value; }
+    }
+
+    public float z {
+        get { return data[2]; }
+        set { data[2] = value; }
+    }
+
+    public FloatVector3() { }
+
+    public FloatVector3(float x, float y, float z) {
+        data[0] = x;
+        data[1] = y;
+        data[2] = z;
+    }
+
+    public float this[int index] {
+        get {
+            // TODO: index check einbauen
+            return data[index];
         }
 
-        public float y {
-            get { return data[1]; }
-            set { data[1] = value; }
+        set {
+            data[index] = value;
         }
+    }
 
-        public float z {
-            get { return data[2]; }
-            set { data[2] = value; }
-        }
+    #region operator
+    public static FloatVector3 operator /(FloatVector3 v1, FloatVector3 v2) {
+        return new FloatVector3(
+            v1.x / v2.x,
+            v1.y / v2.y,
+            v1.z / v2.z
+        );
+    }
 
-        public FloatVector3() { }
+    public static FloatVector3 operator /(FloatVector3 v1, float f) {
+        return new FloatVector3(
+            v1.x / f,
+            v1.y / f,
+            v1.z / f
+        );
+    }
 
-        public FloatVector3(float x, float y, float z) {
-            data[0] = x;
-            data[1] = y;
-            data[2] = z;
-        }
+    public static FloatVector3 operator /(float f, FloatVector3 v1) {
+        return new FloatVector3(
+            v1.x / f,
+            v1.y / f,
+            v1.z / f
+        );
+    }
 
-        public float this[int index] {
-            get {
-                // TODO: index check einbauen
-                return data[index];
-            }
+    public static FloatVector3 operator -(FloatVector3 v1, FloatVector3 v2) {
+        return new FloatVector3(
+            v1.x - v2.x,
+            v1.y - v2.y,
+            v1.z - v2.z
+        );
+    }
 
-            set {
-                data[index] = value;
-            }
-        }
+    public static FloatVector3 operator -(FloatVector3 v1, float f) {
+        return new FloatVector3(
+            v1.x - f,
+            v1.y - f,
+            v1.z - f
+        );
+    }
 
-        #region operator
-        public static FloatVector3 operator /(FloatVector3 v1, FloatVector3 v2) {
-            return new FloatVector3(
-                v1.x / v2.x,
-                v1.y / v2.y,
-                v1.z / v2.z
-            );
-        }
+    public static FloatVector3 operator -(float f, FloatVector3 v1) {
+        return new FloatVector3(
+            v1.x - f,
+            v1.y - f,
+            v1.z - f
+        );
+    }
 
-        public static FloatVector3 operator /(FloatVector3 v1, float f) {
-            return new FloatVector3(
-                v1.x / f,
-                v1.y / f,
-                v1.z / f
-            );
-        }
+    public static FloatVector3 operator *(FloatVector3 v1, FloatVector3 v2) {
+        return new FloatVector3(
+            v1.x * v2.x,
+            v1.y * v2.y,
+            v1.z * v2.z
+        );
+    }
 
-        public static FloatVector3 operator /(float f, FloatVector3 v1) {
-            return new FloatVector3(
-                v1.x / f,
-                v1.y / f,
-                v1.z / f
-            );
-        }
+    public static FloatVector3 operator *(FloatVector3 v1, float f) {
+        return new FloatVector3(
+            v1.x * f,
+            v1.y * f,
+            v1.z * f
+        );
+    }
 
-        public static FloatVector3 operator -(FloatVector3 v1, FloatVector3 v2) {
-            return new FloatVector3(
-                v1.x - v2.x,
-                v1.y - v2.y,
-                v1.z - v2.z
-            );
-        }
+    public static FloatVector3 operator *(float f, FloatVector3 v1) {
+        return new FloatVector3(
+            v1.x * f,
+            v1.y * f,
+            v1.z * f
+        );
+    }
 
-        public static FloatVector3 operator -(FloatVector3 v1, float f) {
-            return new FloatVector3(
-                v1.x - f,
-                v1.y - f,
-                v1.z - f
-            );
-        }
+    public static FloatVector3 operator /(FloatVector3 v1, FloatVector3 v2) {
+        return new FloatVector3(
+            v1.x / v2.x,
+            v1.y / v2.y,
+            v1.z / v2.z
+        );
+    }
 
-        public static FloatVector3 operator -(float f, FloatVector3 v1) {
-            return new FloatVector3(
-                v1.x - f,
-                v1.y - f,
-                v1.z - f
-            );
-        }
+    public static FloatVector3 operator /(FloatVector3 v1, float f) {
+        return new FloatVector3(
+            v1.x / f,
+            v1.y / f,
+            v1.z / f
+        );
+    }
 
-        public static FloatVector3 operator *(FloatVector3 v1, FloatVector3 v2) {
-            return new FloatVector3(
-                v1.x * v2.x,
-                v1.y * v2.y,
-                v1.z * v2.z
-            );
-        }
+    public static FloatVector3 operator -(FloatVector3 v) {
+        return v * -1;
+    }
 
-        public static FloatVector3 operator *(FloatVector3 v1, float f) {
-            return new FloatVector3(
-                v1.x * f,
-                v1.y * f,
-                v1.z * f
-            );
-        }
+    public float dot(FloatVector3 other) {
+        return x * other.x + y * other.y + z * other.z;
+    }
 
-        public static FloatVector3 operator *(float f, FloatVector3 v1) {
-            return new FloatVector3(
-                v1.x * f,
-                v1.y * f,
-                v1.z * f
-            );
-        }
+    public FloatVector3 cross(FloatVector3 other) {
+        return new FloatVector3(
+            y * other.z - z * other.y,
+            z * other.x - x * other.z,
+            x * other.y - y * other.x
+        );
+    }
+    #endregion operator
 
-        public static FloatVector3 operator /(FloatVector3 v1, FloatVector3 v2) {
-            return new FloatVector3(
-                v1.x / v2.x,
-                v1.y / v2.y,
-                v1.z / v2.z
-            );
-        }
+    public float length2() {
+        return x * x + y * y + z * z;
+    }
 
-        public static FloatVector3 operator /(FloatVector3 v1, float f) {
-            return new FloatVector3(
-                v1.x / f,
-                v1.y / f,
-                v1.z / f
-            );
-        }
+    public float length() {
+        return (float) Math.Sqrt(length2());
+    }
 
-        public static FloatVector3 operator -(FloatVector3 v) {
-            return v * -1;
-        }
+    public void normalize() {
+        float len = length();
+        x /= len;
+        y /= len;
+        z /= len;
+    }
 
-        public float dot(FloatVector3 other) {
-            return x * other.x + y * other.y + z * other.z;
-        }
-
-        public FloatVector3 cross(FloatVector3 other) {
-            return new FloatVector3(
-                y * other.z - z * other.y,
-                z * other.x - x * other.z,
-                x * other.y - y * other.x
-            );
-        }
-        #endregion operator
-
-        public float length2() {
-            return x * x + y * y + z * z;
-        }
-
-        public float length() {
-            return (float) Math.Sqrt(length2());
-        }
-
-        public void normalize() {
-            float len = length();
-            x /= len;
-            y /= len;
-            z /= len;
-        }
-
-        public FloatVector3 normalized() {
-            this.normalize();
-            return this;
-        }
+    public FloatVector3 normalized() {
+        this.normalize();
+        return this;
     }
 }
